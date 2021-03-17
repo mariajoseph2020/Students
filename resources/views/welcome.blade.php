294887f85b5c4bd3ff35478de93a79ee332eb967
mariajoseph3030@gmail.com <mca525@rcss.com>
2021-03-17 11:54:51 +0530

    message

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+    <div class="row">
+    <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+    <table class="table">
+    <tr>
+        <td>Name</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Rollno</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Admission No</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>College</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td><button class="btn btn-danger">submit</button></td>
+    </tr>
+    </table>
+    </div>
+    </div>
+    </div>
+</body>
+</html>
